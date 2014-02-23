2c9c867f37b25a9120e468724be3c2291596ac65
Martin Wetterwald <martin@wetterwald.eu>
2014-02-23 11:28:54 +0100

    Implements NetFlux::Tcp::Server::accept.

--- src/Tcp/Server.hpp
+++ src/Tcp/Server.hpp
@@ -15,6 +15,28 @@ namespace NetFlux
 
             bool listen ( int backlog = 10 );
 
+            template <class SERVERSTREAM> SERVERSTREAM * accept ( )
+            {
+                if ( ! * this )
+                {
+                    return 0;
+                }
+
+                sockaddr address;
+                socklen_t address_length = sizeof ( sockaddr );
+                int sockfd = ::accept ( msocket, &address, &address_length );
+
+                if ( sockfd == -1 )
+                {
+#ifdef DEBUG
+                    perror ( "NetFlux::Tcp::Server::accept (accept)" );
+#endif
+                    return 0;
+                }
+
+                return new SERVERSTREAM ( sockfd, address );
+            }
+
         protected:
             Server ( int sock, const InetAddress & address );
         };
