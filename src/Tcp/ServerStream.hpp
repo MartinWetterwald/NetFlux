379453d22dec0bc5fe86641db81b91b438575d1c
Martin Wetterwald <martin@wetterwald.eu>
2014-03-03 20:00:26 +0100

    Fixes SIGPIPE error using MSG_NOSIGNAL flag in Tcp::ServerStream.

--- src/Tcp/ServerStream.hpp
+++ src/Tcp/ServerStream.hpp
@@ -14,12 +14,12 @@ namespace NetFlux
 
             inline ssize_t send ( const void * buf, size_t len )
             {
-                return ::send ( msocket, ( char * ) buf, len, 0 );
+                return ::send ( msocket, ( char * ) buf, len, MSG_NOSIGNAL );
             }
 
             inline ssize_t recv ( void * buf, size_t len )
             {
-                return ::recv ( msocket, ( char * ) buf, len, 0 );
+                return ::recv ( msocket, ( char * ) buf, len, MSG_NOSIGNAL );
             }
 
         protected:
