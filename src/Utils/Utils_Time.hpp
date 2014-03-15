438bed17c66cee4d0a46e69f8993cff9dba4b74c
Martin Wetterwald <martin@wetterwald.eu>
2014-03-16 00:06:10 +0100

    Implements Utils::Time.

---
+++ src/Utils/Utils_Time.hpp
@@ -0,0 +1,20 @@
+#ifndef _UTILS_TIME_HPP_
+#define _UTILS_TIME_HPP_
+
+#include <sys/time.h>
+#include <cstdint>
+
+namespace NetFlux
+{
+    namespace Utils
+    {
+        inline uint64_t getUtime ( )
+        {
+            timeval t;
+            gettimeofday ( &t, nullptr );
+            return uint64_t ( t.tv_sec * 1000000 ) + ( uint64_t ) t.tv_usec;
+        }
+    }
+}
+
+#endif
