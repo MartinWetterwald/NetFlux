356c52cd3f8074cabcc3060369b0cb6dedaed0a5
Martin Wetterwald <martin@wetterwald.eu>
2014-12-16 17:00:34 +0800

    Adds missing send & recv methods in Tcp::Client.

--- src/Tcp/Tcp_Client.hpp
+++ src/Tcp/Tcp_Client.hpp
@@ -14,6 +14,16 @@ namespace NetFlux
 
             bool connectTo ( const std::string & host, uint16_t port );
 
+            inline ssize_t send ( const void * buf, size_t len )
+            {
+                return ::send ( msocket, ( char * ) buf, len, MSG_NOSIGNAL );
+            }
+
+            inline ssize_t recv ( void * buf, size_t len )
+            {
+                return ::recv ( msocket, ( char * ) buf, len, MSG_NOSIGNAL );
+            }
+
         protected:
             Client ( ) = default;
             Client ( SOCKET sock, const InetAddress & address );
